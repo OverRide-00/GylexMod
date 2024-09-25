aa7e71ca630f39a0902be7dbfb3e65831bc59d3a
OverRide-00 <105.OverRide@gmail.com>
2024-09-25 18:23:19 +0100

    new stuff

---
+++ src/client/java/me/override/gylexmod/client/GylexmodClient.java
@@ -0,0 +1,10 @@
+package me.override.gylexmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GylexmodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
